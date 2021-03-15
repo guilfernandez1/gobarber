860743f893ceff2331433f990546e6120371b57b
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2021-03-15 19:19:34 -0300

    Back-End: Autenticação de rota de appointments

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a2428bcb3375a663ea2eeda757d34144',
+    expiresIn: '1d',
+  },
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import apointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', apointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
